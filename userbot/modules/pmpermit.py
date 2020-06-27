00e347f281cf666964879e736081e3e192a87cae
egayghost <59923073+egayghost@users.noreply.github.com>
2020-06-27 14:16:28 +0800

    pmpermit.py
    Customize xD

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -17,8 +17,8 @@ from userbot.events import register
 
 # ========================= CONSTANTS ============================
 UNAPPROVED_MSG = (
-    "`HeY! This is an automated message.\n\n`"
-    "`I haven't approved you to PM yet.`"
+    "`HeY! This is an automated message obviously.\n\n`"
+    "`My Mastor's haven't approved you to PM yet.`"
     "`Please wait for me to look in, I mostly approve PMs.\n\n`"
     "`Until then, please don't spam my Mastor's PM, you'll get blocked and reported if you do so!`")
 # =================================================================
